396bee81658de4c51e1ebfbd1b615378cbbd7d0a
Chathura <32199405+Shenesh@users.noreply.github.com>
2020-11-08 22:56:30 +0530

    11 08 2020

--- resources/views/Notify/create.blade.php
+++ resources/views/Notify/create.blade.php
@@ -6,6 +6,12 @@
 {
  border:1px solid red;
 }
+
+.bgcolor{
+    background: red;
+}
+
+
 </style>
 
 
@@ -23,18 +29,21 @@
         @csrf
         <div class="form-group error">
             <label for="fname">First Name</label>
-            <input type="text" name="fname" id="" value="{{ old('fname') }}" class="form-control">
+            <input type="text" name="fname" id="" value="{{ old('fname') }}" class="form-control error">
         </div>
-        <div class="form-group{{ $errors->has('lname') ? ' error' : '' }}">
+        <div class="form-group">
             <label for="lname">Last Name</label>
-            <input type="text" name="lname" id="" value="{{ old('lname') }}" class="form-control" >
+            <input type="text" name="lname" id="" value="{{ old('lname') }}" class="form-control {{ $errors->has('lname') ? ' is-invalid ' : '' }} " >
         </div>
-        <div class="form-group{{ $errors->has('email') ? ' error' : '' }}">
+        <div class="form-group">
             <label for="email">eMail</label>
-            <input type="email" name="email" id="" value="{{ old('email') }}" class="form-control">
+            <input type="email" name="email" id="" value="{{ old('email') }}" class="form-control {{ $errors->has('email') ? ' is-invalid ' : '' }} ">
         </div>
   
             <input type="submit" value="Save">
+
+            
+
 
 
     </form>
